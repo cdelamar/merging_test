ff26e7c45172442961ed94c55a55232a22458070
cdelamar <contact.clementdelamare@gmail.com>
2024-08-15 13:23:54 +0200

    merging both libs

--- includes/minishell.h
+++ includes/minishell.h
@@ -85,6 +85,7 @@ typedef struct s_token
 	int				index; // 1 2 3
 	struct s_token	*next; // next
 }	t_token;
+
 typedef struct s_cmd
 {
 	int		fd[2];
@@ -145,14 +146,12 @@ int					ft_input_redirect(char **args, int i);
 int					ft_output_redirect(char **args, int i, int append);
 int					ft_heredoc(char *limit);
 
-
 //fd
 void				close_fds(int *fd);
 int					backup_fd(int *saved_stdin, int *saved_stdout);
 void				restore_fd(int saved_stdin, int saved_stdout);
 int					open_file(char *filename, int flags, int mode);
 int					open_heredoc_file(void);
-
 
 //basic executing
 int					handle_exit_command(char *line);
@@ -178,5 +177,96 @@ void sigint_heredoc(int sig);
 int ft_isnumber(char *str);
 
 
+// _____________ PARSING _____________
+
+/* ========== FONCTION ============= */
+
+/*-------------error-----------------*/
+int			check_error(int code_error);
+int			check_error_before_split(char *line);
+int			badchar(char **split_line);
+
+/*-------------libft-----------------*/
+t_token		*token_lstnew(void *content);
+// void		token_lstadd_back(t_token **lst, t_token *new);
+// void		token_lstclear(t_token **lst, void (*del)(void*));
+// int			ft_strcmp(char *s1, char *s2);
+// int			ft_strncmp(char *s1, char *s2, size_t n);
+// size_t		ft_strlen(const char *str);
+int			ft_strlen2(char	*word);
+// char		*ft_substr(const char *s, unsigned int start, size_t len);
+// char		*ft_strchr(char *s, int c);
+int			ft_strchr2(char *s, char c);
+char		*ft_strncpy(char *dst, char *src, size_t n);
+void		ft_bzero(void *s, size_t n);
+char		**ft_split_boosted(char *s);
+int			check_char(char *s, int i, char **lst, int j);
+// int			ft_isalpha(int c);
+// char		*ft_strjoin(char *s1, char *s);
+// char		*ft_strdup(const char *src);
+// char		*ft_strtrim(char const *s1, char const *set);
+size_t		ft_strlcpy(char *dest, const char *src, size_t size);
+char		*ft_strcpy(char *dest, char *src);
+// void		ft_lstdelone(t_token *lst, void (*del)(void*));
+char		*ft_strdup_for_quote(const char *src);
+
+/*------------tools------------------*/
+void		print_node(t_token *token);
+int			nbr_of_strs(char **strs);
+void		print_path(char **evnp);
+void		print_snail(void);
+int			is_qte(char c);
+
+/*------tools of environement--------*/
+int			find_the_dollar(t_token *token);
+int			skip_prefix(char *word);
+char		*skip_sufix(char *word);
+void		is_dollar_interogation(t_token *token_list);
+void		path_other(char **envp, t_token *token_list, int place_of_dollar);
+char		**minisplit_dollar(char *word);
+int			nbr_of_dollar(t_token *token_list, int place_of_dollar);
+int			which_node(int nbr_dollar, t_token *token_list);
+char		**delet_space_to_tab(char **tab_token);
+
+/*new*/
+void		path_main(t_token *token_list, char **envp);
+
+/*------------parsing----------------*/
+void		lexer(t_token *token);
+void		path_main(t_token *token_list, char **envp);
+
+/*------------cat_quote--------------*/
+void		before_node_cat(t_token **token, int i, int verif);
+void		after_node_cat(t_token **token, int i, int verif);
+int			copy_node(t_token *head, t_token *new_node);
+int			copy_node_after(t_token *head, t_token *new_node, int i_content);
+int			copy_mid_node(t_token *head, t_token *new_node, int i_content);
+int			copy_mid_after_node(t_token *head, t_token *new_node);
+void		advance_to_node(t_token **head, t_token **before, int *i);
+char		**delet_quote_inword(t_token **token, t_token *place);
+/*----------lstnew_with_cat---------*/
+char		**mouve_str(char **str, int i);
+int			check_is_space_node(char **str, int i);
+int			lstnew_with_cat(char **str, int i);
+char		**main_cat(t_token **token);
+/*----------tools_for_cat----------*/
+char		**malloc_tab(t_token **token);
+void		cat_quote(char **tab_token, t_token **token);
+
+
+/*--------------free-----------------*/
+void		free_split(char **strs);
+void		free_head(t_token *head);
+void		print_free_tab(char **final_token);
+
+
+size_t		count_word(char *s);
+int			is_char(char s);
+int			skip_space(char *s, int i);
+void		print_split(char **split_line);
+int			solo_quote(char **split_line);
+
+void		signals(void);
+void		sigint_handler(int sig);
 
 #endif
