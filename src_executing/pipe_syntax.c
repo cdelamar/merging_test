76cd33857240cab6ef3258eac30fb99a8fc512c6
cdelamar <contact.clementdelamare@gmail.com>
2024-11-20 07:01:27 +0100

    +| exec norm ok

--- src_executing/pipe_syntax.c
+++ src_executing/pipe_syntax.c
@@ -1,34 +1,44 @@
 #include "../includes/minishell.h"
 
-int	check_all_commands_executable(char ***commands, char **env)
+int	is_command_executable(char **command, char **env)
 {
-	int		i;
 	char	*path;
+
+	if (is_builtin(command[0]))
+		return (1);
+	path = get_cmd_path(command[0], env);
+	if (path == NULL)
+	{
+		if (ft_strcmp(command[0], "<") == 0
+			|| ft_strcmp(command[0], ">") == 0
+			|| ft_strcmp(command[0], "<<") == 0
+			|| ft_strcmp(command[0], ">>") == 0)
+			printf("redirections : syntax error\n");
+		else
+			fprintf(stderr, "%s: command not found\n", command[0]);
+		return (0);
+	}
+	free(path);
+	return (1);
+}
+
+int	validate_commands(char ***commands, char **env)
+{
+	int	i;
 
 	i = 0;
 	while (commands[i] != NULL)
 	{
-		if (is_builtin(commands[i][0]))
-		{
-			i++;
-			continue ;
-		}
-		path = get_cmd_path(commands[i][0], env);
-		if (path == NULL)
-		{
-			if (ft_strcmp(commands[i][0], "<") == 0 ||
-				ft_strcmp(commands[i][0], ">") == 0 ||
-				ft_strcmp(commands[i][0], "<<") == 0 ||
-				ft_strcmp(commands[i][0], ">>") == 0)
-				printf("redirections : syntax error\n");
-			else
-				fprintf(stderr, "%s: command not found\n", commands[i][0]);
+		if (!is_command_executable(commands[i], env))
 			return (0);
-		}
-		free(path);
 		i++;
 	}
 	return (1);
+}
+
+int	check_all_commands_executable(char ***commands, char **env)
+{
+	return (validate_commands(commands, env));
 }
 
 void	execute_builtin_child(t_cmd *cmd, char ***commands)
